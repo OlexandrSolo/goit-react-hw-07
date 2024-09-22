42b1619b2271aac6bdd5fed96a99804f034a0d36
Your Name <you@example.com>
2024-09-22 14:50:07 +0300

    complited all tasks

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,22 +2,14 @@ import { useSelector } from "react-redux";
 import style from "./ContactList.module.css";
 
 import ContactListItem from "../Contact/Contact";
-import { selectContacts, selectFilter } from "../../redux/selectors";
-
-const getFilteredList = (contactList, filter) => {
-  return contactList.filter((item) =>
-    item.name.toLowerCase().includes(filter.toLowerCase())
-  );
-};
+import { selectVisibleContact } from "../../redux/selectors";
 
 export default function ContactList() {
-  const contactList = useSelector(selectContacts);
-  const filterName = useSelector(selectFilter);
-  const list = getFilteredList(contactList, filterName);
+  const listContact = useSelector(selectVisibleContact);
 
   return (
     <ul>
-      {list.map((user) => (
+      {listContact.map((user) => (
         <li key={user.id} className={style.contact}>
           <ContactListItem user={user} />
         </li>
